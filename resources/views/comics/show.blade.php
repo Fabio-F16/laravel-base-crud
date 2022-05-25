f700c0a3570e61f635d9055b98fc3658aea46dae
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-25 15:42:04 +0200

    show page

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title')
+    dettaglio fumetto
+@endsection
+
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="col-12 text-center">{{ $comic->title }}</h1>
+            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+            <p>{{ $comic->description }}</p>
+            <h5>Price: {{ $comic->price }}</h5>
+        </div>
+        <a href="{{ route('comics.index') }}">Torna alla lista dei fumetti!</a>
+    </div>
+@endsection
