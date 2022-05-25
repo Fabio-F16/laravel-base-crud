0945137d6e7d3d8ac1179933db66970d3e6468a0
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-25 15:41:57 +0200

    index page

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title')
+    laravel-base-crud
+@endsection
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @forelse ($comics as $item)
+                <div class="card col-12 col-md-4">
+                    <img src="{{ $item->thumb }}" alt="{{ $item->title }}">
+                    <h3> <a href="{{ route('comics.show', $item->id) }}">{{ $item->title }}</a></h3>
+                    {{-- <p>{{ $item->description }}</p> --}}
+                    <p>Price: {{ $item->price }}</p>
+                    <p>Sales: {{ $item->sale_date }}</p>
+                </div>
+
+            @empty
+                <h1>Non ci sono fumetti disponibili</h1>
+            @endforelse
+        </div>
+    </div>
+@endsection
