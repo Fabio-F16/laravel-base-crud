9dab8196ffdfc84278e51b72acbe282857fec9c9
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-26 17:50:03 +0200

    link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
     <title> @yield('title')</title>
 </head>
 
 <body>
     @yield('content')
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
