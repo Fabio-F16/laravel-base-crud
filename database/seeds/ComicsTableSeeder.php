7ff81c24f8dd4fff0e93b3756f4bd7da8ee46e3b
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-26 14:26:03 +0200

    add fillable

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -14,14 +14,16 @@ class ComicsTableSeeder extends Seeder
         //
         $data = config('comics');
         foreach ($data as $item) {
-            $newComic = new Comic();
-            $newComic->title = $item['title'];
-            $newComic->description = $item['description'];
-            $newComic->thumb = $item['thumb'];
-            $newComic->price = $item['price'];
-            $newComic->series = $item['series'];
-            $newComic->sale_date = $item['sale_date'];
-            $newComic->type = $item['type'];
+            // $newComic = new Comic();
+            // $newComic->title = $item['title'];
+            // $newComic->description = $item['description'];
+            // $newComic->thumb = $item['thumb'];
+            // $newComic->price = $item['price'];
+            // $newComic->series = $item['series'];
+            // $newComic->sale_date = $item['sale_date'];
+            // $newComic->type = $item['type'];
+            // avessi la stessa relazione dei nomi tra nome colonne e nome dei dati da inserire, (dopo aver modificato il model comic con fillable) posso utilizzare il metodo:
+            $pasta->fill($data);
             $newComic->save();
         }
     }
